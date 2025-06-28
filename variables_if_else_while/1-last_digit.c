b3e0cb5c504e8202a5734944a2b66c7f7c3a9186
Affracs <alexfrancois1501@gmail.com>
2025-06-28 20:44:49 +0000

    sdfsfgsdf

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 /**
  * main - Complete the source code in order to print the last digit of the number stored in the variable n.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
