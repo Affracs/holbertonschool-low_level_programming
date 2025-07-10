4f5614026caf294044aa4425466f9da826b32ba8
Affracs <alexfrancois1501@gmail.com>
2025-07-10 10:54:48 +0000

    2 try 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (s != '0/')
+	while (*s != '\0')
 	{
-		(*s == c)
+		if (*s == c)
 			return (s);
 		s++;
 	}
 
 	if (c == '\0')
 		return (s);
-
 	return (0);
 }
