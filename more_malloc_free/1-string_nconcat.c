88026d3b6e4d2618e6ff37ebc43f1d96198ce6d6
Affracs <alexfrancois1501@gmail.com>
2025-07-20 15:21:29 +0000

    1-1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -6,12 +6,36 @@
  *
  *
  */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *ptr = malloc(b);
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *concat;
 
-	if (ptr == NULL)
-		exit(98);
+	if (s1)
+		while (s1[len1])
+			len1++;
+	if (s2)
+		while (s2[len2])
+			len2++;
 
-	return (ptr);
-} 
+	if (n >= len2)
+		n = len2;
+
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (!concat)
+		return (NULL);
+
+	while (i < len1)
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	while (j < n)
+	{
+		concat[i + j] = s2[j];
+		j++;
+	}
+	concat[i + j] = '\0';
+
+	return (concat);
+}
