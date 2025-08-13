11a6e68b011fedaac42ac3299c5e799476fa732b
Affracs <alexfrancois1501@gmail.com>
2025-08-13 06:16:48 +0000

    Remade the file '6-puts2.c'(supposedly betty compliant)

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer the the string being printed
+ *
+ * Retunr: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar (str[i]);
+		i += 2;
+	}
+}
