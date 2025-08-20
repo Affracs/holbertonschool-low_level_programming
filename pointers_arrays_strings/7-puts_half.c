92bb5ec57a1170841f17a43bc1bd5cc37823a5db
Affracs <alexfrancois1501@gmail.com>
2025-08-20 09:32:51 +0000

    fixed

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,31 @@
 #include "main.h"
 
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+        i++;
+
+    return i;
+}
+
+/**
+ * puts_half - prints half of a string with a newline
+ * @str: string to print
+ *
+ * Return: Void (0)
+ */
+
 void puts_half(char *str)
 {
-	int len = _strlen(str);
-	int half = (len + 1) / 2;
-	
-	while (str[half] != '\0')
-	{
-		_putchar(str[half]);
-		half++;
-	}
-	_putchar('\n');
+        int len = _strlen(str);
+        int half = (len + 1) / 2;
+
+        while (str[half] != '\0')
+        {
+                _putchar(str[half]);
+                half++;
+        }
+        _putchar('\n');
 }
