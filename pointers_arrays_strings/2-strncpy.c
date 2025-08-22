ca7b08bcf03df92d9e536d03da6a51c989c98efd
Affracs <alexfrancois1501@gmail.com>
2025-08-22 10:19:39 +0000

    fixed

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,11 +4,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0; 
 	
-	while (i < n && src[i] != 0)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return dest;
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
