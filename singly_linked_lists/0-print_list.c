c213a32bff86d1dc52de034b12a10b7cb292f7bf
Affracs <alexfrancois1501@gmail.com>
2025-09-02 08:38:49 +0000

    compield

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 size_t print_list(const list_t *h)
 {
--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,37 +2,15 @@
 #include <string.h>
 #include "lists.h"
 
-size_t list_len(const list_s *h)
+size_t list_len(const list_t *h)
 {
-        list_t *new_node;
-        unsigned int len = 0;
+    size_t count = 0;
 
-        if (str == NULL)
-        {
-                return (NULL);
-        }
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
 
-        new_node = malloc(sizeof(list_t));
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
-
-        new_node-> = strdup(str);
-        if (new_node->str == NULL)
-        {
-                free(new_node);
-                return (NULL);
-        }
-
-        while (str[len])
-        {
-                len++;
-        }
-
-        new_node->len = len;
-        new_node->next = *head;
-        *head = new_node;
-
-        return (new_node);
+    return count;
 }
