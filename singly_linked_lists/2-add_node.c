45778ca7b7759ada4397e4462cee50992c63cb93
Affracs <alexfrancois1501@gmail.com>
2025-09-02 10:31:46 +0000

    f

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,15 +2,27 @@
 #include <string.h>
 #include "lists.h"
 
-size_t list_len(const list_t *h)
+list_t *add_node(list_t **head, const char *str)
 {
-    size_t count = 0;
+    list_t *new_node;
 
-    while (h != NULL)
+    if (!head || !str)
+        return (NULL);
+
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (!new_node->str)
     {
-        count++;
-        h = h->next;
+        free(new_node);
+        return (NULL);
     }
 
-    return count;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
 }
